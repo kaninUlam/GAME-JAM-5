a6b26212eaf69e6bdea3770eb60eba2be9b2f3ae
John Allen <valerio.allen24@gmail.com>
2021-10-26 14:11:15 +1300

    Has movement
    test scene for game jam has movement

---
+++ Assets/John/Script/characterStat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterStat : MonoBehaviour
+{
+    public float speed = 10;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.position = transform.position +  new Vector3(horizontalInput * Time.deltaTime * speed, 0, verticalInput * Time.deltaTime * speed);
+    }
+}
