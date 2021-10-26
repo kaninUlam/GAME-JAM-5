96dcab4240d65105b8a96fa158d6f745fb99d56f
John Allen <valerio.allen24@gmail.com>
2021-10-26 15:22:56 +1300

    Item return and item get working
    items are working ui needs work

--- Assets/John/Script/characterStat.cs
+++ Assets/John/Script/characterStat.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class characterStat : MonoBehaviour
 {
-    public float speed = 10;
+    int jumpNum = 1;
 
+    public float speed = 10;
+    public float jumpHeight = 10;
+
+    public bool hasItem = false;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -19,5 +23,27 @@ public class characterStat : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         transform.position = transform.position +  new Vector3(horizontalInput * Time.deltaTime * speed, 0, verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown("space") && jumpNum >= 1)
+        {
+            Debug.Log("Jumped");
+            rb.AddForce(Vector3.up * jumpHeight,ForceMode.Impulse);
+            jumpNum--;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "platform")
+        {
+            jumpNum = 1;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Item")
+        {
+            Debug.Log("pickup");
+            hasItem = true;
+        }
     }
 }
---
+++ Assets/John/Script/returnItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class returnItem : MonoBehaviour
+{
+    characterStat item;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Capsule");
+        item = player.GetComponent<characterStat>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (item.hasItem == true)
+            {
+                Debug.Log("you returned item");
+                item.hasItem = false;
+            }
+        }
+    }
+}
