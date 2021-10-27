2f13624ebf78f644653bbf642a11e63e1884a2a0
John Allen <valerio.allen24@gmail.com>
2021-10-27 15:30:27 +1300

    3D Models
    Added 3D models for building and bridge
    changed the return item to needing input from player

--- Assets/John/Script/returnItem.cs
+++ Assets/John/Script/returnItem.cs
@@ -6,22 +6,45 @@ public class returnItem : MonoBehaviour
 {
     characterStat item;
     GameObject player;
+
+    bool InZone = false;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Capsule");
         item = player.GetComponent<characterStat>();
-    }
 
+        InZone = false;
+    }
+    private void Update()
+    {
+        if(InZone == true && Input.GetKeyDown(KeyCode.E))
+        {
+            if(item.hasItem == true)
+            {
+                Debug.Log("has item");
+                item.hasItem = false;
+            }
+            else
+            {
+                Debug.Log("no item");
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            if (item.hasItem == true)
-            {
-                Debug.Log("you returned item");
-                item.hasItem = false;
-            }
+            Debug.Log("player in zone");
+            InZone = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("player exited the zone");
+            InZone = false;
         }
     }
 }
