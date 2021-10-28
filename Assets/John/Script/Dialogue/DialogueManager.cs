49cf48a2b5771aa1efc1885376fdd27291525bc0
John Allen <valerio.allen24@gmail.com>
2021-10-29 12:41:59 +1300

    added tileable background

---
+++ Assets/John/Script/Dialogue/DialogueManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Queue<string> dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = new Queue<string>();
+    }
+}
